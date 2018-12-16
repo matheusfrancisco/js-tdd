875d7b516e62a3137e5bb3640e693a35a8d248af
matheusfrancisco <mfranciscobatistamachado@gmail.com>
2018-12-16 17:11:37 -0200

    use export in utils

---
+++ examples/utils.js
@@ -0,0 +1,18 @@
+function sum(a,b) {
+    return a+b;
+}
+
+export function mult(a,b) {
+    return a+b;
+}
+
+
+function sub(a,b) {
+    return a -b;
+}
+
+function div(a,b) {
+    return a/b;
+}
+export {sub , div};
+export default sum;
